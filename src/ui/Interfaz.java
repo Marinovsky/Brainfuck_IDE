9bdf5814101357f922503f761c3babfc94aaecde
JohnnyDingo <44392906+JohnnyDingo@users.noreply.github.com>
2020-04-06 11:14:52 -0500

    se añadieron atajos de teclado y se corrigieron errores
    errores corregidos:
    -no se asignaba el color al rehacer simbolos
    -no se guardaba la informacion al rehacer o deshacer
    - no se guardaba texto seleccionado y eliminado por backspace y supr.
    - no se guardaba caracteres seleccionados y eliminados por backspace y supr.
    
    errores conocidos:
    -deshacer y rehacer solo funcionan cuando el cursor queda al final del texto

--- src/ui/Interfaz.java
+++ src/ui/Interfaz.java
@@ -11,6 +11,7 @@ import java.awt.Font;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.InputEvent;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import javax.swing.BorderFactory;
@@ -24,6 +25,7 @@ import javax.swing.JPanel;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.JTextPane;
+import javax.swing.KeyStroke;
 import javax.swing.text.BadLocationException;
 import javax.swing.text.Style;
 import javax.swing.text.StyleConstants;
@@ -52,8 +54,10 @@ public final class Interfaz implements ActionListener{
     Color color5 = new Color(247, 175, 0);  //mostaza
     Color color6 = new Color(68, 48, 0);    //mostaza oscuro
     Font fuente1 = new Font("Console", Font.PLAIN, Py(40));
-    Pila<String> undo = new Pila<String>();
-    Pila<String> redo = new Pila<String>();
+    StyledDocument doc;
+    Style estilo;
+    Pila<String> undo = new Pila<>();
+    Pila<String> redo = new Pila<>();
     
     //ventana
     void crearVentana(){
@@ -69,40 +73,44 @@ public final class Interfaz implements ActionListener{
     }
     void crearBarraMenus(){
         BarraMenus= new JMenuBar();
+        BarraMenus.setBackground(Color.WHITE);
+        
         MenuArchivo=new JMenu("Archivo");
         BarraMenus.add(MenuArchivo);
+        crearMenuArchivo();
+        
         MenuCorrer=new JMenu("Correr");
         BarraMenus.add(MenuCorrer);
         
         MenuEditar=new JMenu("Editar");
-        MenuEditar.addActionListener(this);
         BarraMenus.add(MenuEditar);
-        
-        BarraMenus.setBackground(Color.WHITE);
-        crearMenuArchivo();
         crearMenuEditar();
     }
     void crearMenuArchivo(){
         subMenuNuevo = new JMenuItem("Nuevo");
         subMenuNuevo.addActionListener(this);
+        subMenuNuevo.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N, InputEvent.CTRL_MASK));
         MenuArchivo.add(subMenuNuevo);
         
         subMenuAbrir = new JMenuItem("Abrir");
         subMenuAbrir.addActionListener(this);
+        subMenuAbrir.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_O, InputEvent.CTRL_MASK));
         MenuArchivo.add(subMenuAbrir);
         
         subMenuGuardar = new JMenuItem("Guardar");
         subMenuGuardar.addActionListener(this);
+        subMenuGuardar.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, InputEvent.CTRL_MASK));
         MenuArchivo.add(subMenuGuardar);
     }
-    
     void crearMenuEditar(){
         retroceder = new JMenuItem("Retroceder");
         retroceder.addActionListener(this);
+        retroceder.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_Z, InputEvent.CTRL_MASK));
         MenuEditar.add(retroceder);
         
         avanzar = new JMenuItem("Avanzar");
         avanzar.addActionListener(this);
+        avanzar.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_Y, InputEvent.CTRL_MASK));
         MenuEditar.add(avanzar);
     }
     
@@ -114,8 +122,8 @@ public final class Interfaz implements ActionListener{
         PanelProgramacion.setLocation(x, y);
         
         CuadroProgramacion = new JTextPane();
-        StyledDocument doc = CuadroProgramacion.getStyledDocument();
-        Style style = CuadroProgramacion.addStyle("", null);
+        doc = CuadroProgramacion.getStyledDocument();
+        estilo = CuadroProgramacion.addStyle(null, null);
         CuadroProgramacion.setBounds(0, Py(35), Px(1880), Py(575));
         CuadroProgramacion.setFont(fuente1);
         CuadroProgramacion.setBorder(BorderFactory.createLineBorder(color4, Px(4), true));
@@ -123,62 +131,32 @@ public final class Interfaz implements ActionListener{
         CuadroProgramacion.addKeyListener(new KeyAdapter() {
             @Override
             public void keyTyped(KeyEvent e) {
-                char c = e.getKeyChar();
-                switch (c) {
-                    case '<':
-                        StyleConstants.setForeground(style, Color.black);
-                        try {
-                            doc.insertString(doc.getLength(), "<",style); 
-                            doc.remove(doc.getLength()-1, 1);
-                            undo.push(String.valueOf(c));
-                        }
-                        catch (BadLocationException b){}
-                        break;
-                    case '>':
-                        StyleConstants.setForeground(style, Color.black);
-                        try { doc.insertString(doc.getLength(), ">",style);
-                              doc.remove(doc.getLength()-1, 1);
-                              undo.push(String.valueOf(c));
-                        }
-                        catch (BadLocationException b){}
-                        break;
-                    case '.':
-                        StyleConstants.setForeground(style, Color.black);
-                        try { doc.insertString(doc.getLength(), ".",style); doc.remove(doc.getLength()-1, 1);undo.push(String.valueOf(c));}
-                        catch (BadLocationException b){}
-                        break;
-                    case '+':
-                        StyleConstants.setForeground(style, Color.black);
-                        try { doc.insertString(doc.getLength(), "+",style); doc.remove(doc.getLength()-1, 1);undo.push(String.valueOf(c));}
-                        catch (BadLocationException b){}
-                        break;
-                    case '-':
-                        StyleConstants.setForeground(style, Color.black);
-                        try { doc.insertString(doc.getLength(), "-",style); doc.remove(doc.getLength()-1, 1);undo.push(String.valueOf(c));}
-                        catch (BadLocationException b){}
-                        break;
-                    case '[':
-                        StyleConstants.setForeground(style, Color.black);
-                        try { doc.insertString(doc.getLength(), "[",style); doc.remove(doc.getLength()-1, 1);undo.push(String.valueOf(c));}
-                        catch (BadLocationException b){}
-                        break;
-                    case ']':
-                        StyleConstants.setForeground(style, Color.black);
-                        try { doc.insertString(doc.getLength(), "]",style); doc.remove(doc.getLength()-1, 1);undo.push(String.valueOf(c));}
-                        catch (BadLocationException b){}
-                        break;
-                    case ';':
-                        StyleConstants.setForeground(style, Color.black);
-                        try { doc.insertString(doc.getLength(), ";",style); doc.remove(doc.getLength()-1, 1);undo.push(String.valueOf(c));}
-                        catch (BadLocationException b){}
-                        break;
-                    case ':':
-                        StyleConstants.setForeground(style, Color.black);
-                        try { doc.insertString(doc.getLength(), ":",style); doc.remove(doc.getLength()-1, 1);undo.push(String.valueOf(c));}
-                        catch (BadLocationException b){}
-                        break;
-                    default:
-                        e.consume();
+                String simbolo = String.valueOf(e.getKeyChar());
+                e.consume();
+                if(simbolo.equals("<")||
+                        simbolo.equals(">")||
+                        simbolo.equals(".")|| 
+                        simbolo.equals("+")|| 
+                        simbolo.equals("-")|| 
+                        simbolo.equals("[")|| 
+                        simbolo.equals("]")|| 
+                        simbolo.equals(";")||
+                        simbolo.equals(":")){
+                    imprimirColorSimbolo(simbolo);
+                    undo.push("e"+simbolo);
+                    redo.reset();
+                }
+            }
+            @Override
+            public void keyPressed (KeyEvent e){
+                if(e.getKeyCode()==KeyEvent.VK_BACK_SPACE || e.getKeyCode()==KeyEvent.VK_DELETE){
+                    if(CuadroProgramacion.getSelectedText()!=null)
+                        undo.push("b"+CuadroProgramacion.getSelectedText());
+                    else {
+                        try{
+                            undo.push("b"+doc.getText(doc.getLength()-1, 1));
+                        }catch(BadLocationException b){}
+                    }
                 }
             }
         });
@@ -187,6 +165,8 @@ public final class Interfaz implements ActionListener{
         etiqueta = new JLabel("Nuevo");
         etiqueta.setBounds(Px(10), 0, Px(150), Py(35));
         PanelProgramacion.add(etiqueta);
+        
+        Ventana.getContentPane().add(PanelProgramacion);
     }
     void CrearPanelEntrada (int x, int y, int ancho, int alto){
         PanelEntrada = new JPanel();
@@ -203,6 +183,8 @@ public final class Interfaz implements ActionListener{
         etiqueta = new JLabel("Entrada");
         etiqueta.setBounds(Px(10), 0, Px(150), Py(35));
         PanelEntrada.add(etiqueta);
+        
+        Ventana.getContentPane().add(PanelEntrada);
     }
     void CrearPanelSalida (int x, int y, int ancho, int alto){
         PanelSalida = new JPanel();
@@ -220,17 +202,32 @@ public final class Interfaz implements ActionListener{
         etiqueta = new JLabel("Salida");
         etiqueta.setBounds(Px(10), 0, Px(150), Py(35));
         PanelSalida.add(etiqueta);
+        
+        Ventana.getContentPane().add(PanelSalida);
     }
     
-    //funciones
+    /*funciones
+    Retro(deshacer):
+        - si caracter[0] es "e" entonces el usuario habia [escrito] deshacer será borrar
+        - si caracter[0] es "b" entonces el usuario habia [borrado] deshacer será imprimir
+        - guarda en pila "redo"
+    Avanzar(rehacer):
+        - si caracter[0] es "e" entonces el usuario habia [escrito] deshacer será imprimir
+        - si caracter[0] es "b" entonces el usuario habia [borrado] deshacer será borrar
+        - guarda en pila "undo"
+    colorSimbolo:
+        - cambia los colores a los simbolos e imprime en "CuadroProgramacion" */
+    int Px(int pixeles){
+        return resolucion.width*pixeles/1920;
+    }
+    int Py(int pixeles){
+        return resolucion.height*pixeles/1080;
+    }
     void subMenuNuevo(){
         Ventana.getContentPane().setLayout(null);
         CrearPanelProgramacion(Px(20), Py(20), Px(1880), Py(610));
-        Ventana.getContentPane().add(PanelProgramacion);
         CrearPanelEntrada(Px(20), Py(650), Px(930), Py(340));
-        Ventana.getContentPane().add(PanelEntrada);
         CrearPanelSalida(Px(970), Py(650), Px(930), Py(340));
-        Ventana.getContentPane().add(PanelSalida);
         Ventana.repaint();
     }
     void subMenuAbrir(){
@@ -238,33 +235,72 @@ public final class Interfaz implements ActionListener{
         fd.setVisible(true);
     }
     void Retro(){
-        StyledDocument doc = CuadroProgramacion.getStyledDocument();
-        Style style = CuadroProgramacion.addStyle("", null);
-        CuadroProgramacion.setBounds(0, Py(35), Px(1880), Py(575));
-        CuadroProgramacion.setFont(fuente1);
-        CuadroProgramacion.setBorder(BorderFactory.createLineBorder(color4, Px(4), true));
-        CuadroProgramacion.setBackground(color3);
-        redo.push(undo.pop());
-        try{
-            doc.remove(doc.getLength()-1, 1);
-        }catch(BadLocationException b){};
+        if(!undo.empty()){
+            String cadena = undo.pop();
+            redo.push(cadena);
+            if(cadena.substring(0,1).equals("e"))
+                try{
+                    doc.remove(doc.getLength()-1, 1);
+                }catch(BadLocationException b){}
+            if(cadena.substring(0,1).equals("b"))
+                for(int i=1; i<cadena.length(); i++)
+                    imprimirColorSimbolo(cadena.substring(i,i+1));
+        }else Toolkit.getDefaultToolkit().beep();
     }
     void Adelante(){
-        StyledDocument doc = CuadroProgramacion.getStyledDocument();
-        Style style = CuadroProgramacion.addStyle("", null);
-        CuadroProgramacion.setBounds(0, Py(35), Px(1880), Py(575));
-        CuadroProgramacion.setFont(fuente1);
-        CuadroProgramacion.setBorder(BorderFactory.createLineBorder(color4, Px(4), true));
-        CuadroProgramacion.setBackground(color3);
-        try{
-            doc.insertString(doc.getLength(), redo.pop(), style);
-        }catch(BadLocationException b){};
+        if(!redo.empty()){
+            String cadena =redo.pop();
+            undo.push(cadena);
+            if(cadena.substring(0,1).equals("e"))
+                for(int i=1; i<cadena.length(); i++)
+                    imprimirColorSimbolo(cadena.substring(i,i+1));
+            if(cadena.substring(0,1).equals("b"))
+                try{
+                    doc.remove(doc.getLength()-(cadena.length()-1), cadena.length()-1);
+                }catch(BadLocationException b){}
+        }else Toolkit.getDefaultToolkit().beep();
     }
-    int Px(int pixeles){
-        return resolucion.width*pixeles/1920;
-    }
-    int Py(int pixeles){
-        return resolucion.height*pixeles/1080;
+    void imprimirColorSimbolo(String simbolo){
+        try {
+            switch (simbolo) {
+                case "<":
+                    StyleConstants.setForeground(estilo, Color.black);
+                    doc.insertString(doc.getLength(), simbolo, estilo);
+                    break;
+                case ">":
+                    StyleConstants.setForeground(estilo, Color.black);
+                    doc.insertString(doc.getLength(), simbolo, estilo);
+                    break;
+                case ".":
+                    StyleConstants.setForeground(estilo, Color.black);
+                    doc.insertString(doc.getLength(), simbolo, estilo);
+                    break;
+                case "+":
+                    StyleConstants.setForeground(estilo, Color.black);
+                    doc.insertString(doc.getLength(), simbolo, estilo);
+                    break;
+                case "-":
+                    StyleConstants.setForeground(estilo, Color.black);
+                    doc.insertString(doc.getLength(), simbolo, estilo);
+                    break;
+                case "[":
+                    StyleConstants.setForeground(estilo, Color.black);
+                    doc.insertString(doc.getLength(), simbolo, estilo);
+                    break;
+                case "]":
+                    StyleConstants.setForeground(estilo, Color.black);
+                    doc.insertString(doc.getLength(), simbolo, estilo);
+                    break;
+                case ";":
+                    StyleConstants.setForeground(estilo, Color.black);
+                    doc.insertString(doc.getLength(), simbolo, estilo);
+                    break;
+                case ":":
+                    StyleConstants.setForeground(estilo, Color.black);
+                    doc.insertString(doc.getLength(), simbolo, estilo);
+                    break;
+            }
+        }catch (BadLocationException b){}
     }
     
     //Constructor
@@ -275,12 +311,11 @@ public final class Interfaz implements ActionListener{
     
     @Override
     public void actionPerformed(ActionEvent e) {
-        String command = e.getActionCommand();
-        switch(command){
-            case "Nuevo": 
+        switch(e.getActionCommand()){
+            case "Nuevo":
                 subMenuNuevo();
                 break;
-            case "Abrir": 
+            case "Abrir":
                 subMenuAbrir();
                 break;
             case "Retroceder":
